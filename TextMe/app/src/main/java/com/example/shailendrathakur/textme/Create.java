6c9149e7cf310d24d674651dfad4028223156941
Shailendra-Java <shailendrasingh2011@live.in>
2018-01-16 15:25:59 +0530

    Text_Me
    Android messanger app

--- TextMe/app/src/main/java/com/example/shailendrathakur/textme/Create.java
+++ TextMe/app/src/main/java/com/example/shailendrathakur/textme/Create.java
@@ -1,12 +1,18 @@
 package com.example.shailendrathakur.textme;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +24,9 @@ import android.widget.Toast;
  * Created by Shailendra Thakur on 16-01-2018.
  */
 
-public class Create extends Activity {
+public class Create extends Activity implements View.OnClickListener{
 
+    public static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
     EditText mobileno, message;
     Button sendsms;
     TextView stat;
@@ -29,36 +36,86 @@ public class Create extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.create);
 
         mobileno = (EditText) findViewById(R.id.editText1);
         message = (EditText) findViewById(R.id.editText2);
         stat = (TextView)findViewById(R.id.status);
         sendsms = (Button) findViewById(R.id.button1);
-        sendsms.setOnClickListener(new View.OnClickListener() {
+        sendsms.setOnClickListener(this);
 
-            @Override
-            public void onClick(View arg0) {
-                String SENT = "SMS_SENT";
-                String DELIVERED = "SMS_DELIVERED";
-
-                String no = mobileno.getText().toString();
-                String msg = message.getText().toString();
-
-                PendingIntent sentPI = PendingIntent.getBroadcast(Create.this, 0, new Intent(SENT), 0);
-                PendingIntent deliveredPI = PendingIntent.getBroadcast(Create.this, 0, new Intent(DELIVERED), 0);
-
-                registerReceiver(sendBroadcastReceiver, new IntentFilter(SENT));
-                registerReceiver(deliveryBroadcastReciever, new IntentFilter(DELIVERED));
-
-                SmsManager sms = SmsManager.getDefault();
-                sms.sendTextMessage(no, null, msg, sentPI, deliveredPI);
-
-                Toast.makeText(Create.this, "Message sent successfully", Toast.LENGTH_SHORT).show();
+        if (ContextCompat.checkSelfPermission(Create.this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Create.this,
+                    Manifest.permission.SEND_SMS))
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setPositiveButton("App wants permission for Sending SMS", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(Create.this,"Thank You",Toast.LENGTH_LONG).show();
+                    }
+                });
             }
-        });
-    }
+            else
+            {
+                ActivityCompat.requestPermissions(Create.this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
 
+            }
+        }
+
+    }
+    @Override
+    public void onClick(View arg0) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        String no = mobileno.getText().toString();
+        String msg = message.getText().toString();
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(Create.this, 0, new Intent(SENT), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(Create.this, 0, new Intent(DELIVERED), 0);
+
+        registerReceiver(sendBroadcastReceiver, new IntentFilter(SENT));
+        registerReceiver(deliveryBroadcastReciever, new IntentFilter(DELIVERED));
+
+        if (ContextCompat.checkSelfPermission(Create.this,
+                Manifest.permission.SEND_SMS)
+                == PackageManager.PERMISSION_GRANTED) {
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(no, null, msg, sentPI, deliveredPI);
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    try {
+                        onClick(sendsms);
+                    }catch(Exception exp){
+                        Toast.makeText(Create.this, "Some problem occurs", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(Create.this,"Permission Denied!",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
     @Override
     protected void onDestroy() {
         // TODO Auto-generated method stub
--- TextMe/app/src/main/java/com/example/shailendrathakur/textme/Draft.java
+++ TextMe/app/src/main/java/com/example/shailendrathakur/textme/Draft.java
@@ -1,12 +1,19 @@
 package com.example.shailendrathakur.textme;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 /**
  * Created by Shailendra Thakur on 16-01-2018.
@@ -14,6 +21,7 @@ import android.widget.SimpleCursorAdapter;
 
 public class Draft extends Activity {
 
+    public static final int MY_PERMISSIONS_REQUEST_READ_SMS = 0;
     ListView inbx;
     SimpleCursorAdapter adapter;
     @Override
@@ -22,8 +30,32 @@ public class Draft extends Activity {
         super.onCreate(bundle);
         setContentView(R.layout.inbox);
         inbx = (ListView)findViewById(R.id.lvMsg);
+
+        if (ContextCompat.checkSelfPermission(Draft.this,
+                Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Draft.this,
+                    Manifest.permission.READ_SMS))
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setPositiveButton("App wants permission for Read Draft", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(Draft.this,"Thank You",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(Draft.this,
+                        new String[]{Manifest.permission.READ_SMS},
+                        MY_PERMISSIONS_REQUEST_READ_SMS);
+
+            }
+        }
+        loadDraft();
     }
-    private void loadInbox()
+    private void loadDraft()
     {
         Uri inboxURI = Uri.parse("content://sms/draft");
 
@@ -37,5 +69,31 @@ public class Draft extends Activity {
                 new String[] { "body", "address" }, new int[] {
                 R.id.msg, R.id.lbl });
         inbx.setAdapter(adapter);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_SMS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    try {
+                        loadDraft();
+                    }catch(Exception exp){
+                        Toast.makeText(Draft.this, "Some problem occurs", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(Draft.this,"Permission Denied!",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
